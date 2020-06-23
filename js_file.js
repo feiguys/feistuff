af2c25b1d7c9cbc4560ae9b6bc49e40722ae28d8
feiguys <66875761+feiguys@users.noreply.github.com>
2020-06-23 19:16:59 +0200

    Update js_file.js

--- js_file.js
+++ js_file.js
@@ -1,15 +1,1 @@
-<<<<<<< HEAD
-=======
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
->>>>>>> 709100352b16e0b4f6243634645f6a31e4e0fb9f
