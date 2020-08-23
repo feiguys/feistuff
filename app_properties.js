dcd4fa1275127a11dee523814f29e8c301a1beff
Zoltán Skultéty <40636200+skuzol@users.noreply.github.com>
2020-08-23 21:37:33 +0200

    Create app_properties.js

---
+++ app_properties.js
@@ -0,0 +1,15 @@
+         const options = {
+            bottom: '64px', // default: '32px'
+            right: '32px', // default: '32px'
+            time: '0s', // default: '0.3s'
+            mixColor: '#eeeeee', // default: '#fff'
+            backgroundColor: '#eeeeee',  // default: '#fff'
+            buttonColorDark: '#100f2c',  // default: '#100f2c'
+            buttonColorLight: '#eeeeee', // default: '#fff'
+            saveInCookies: false, // default: true,
+            label: '🌓', // default: ''
+            autoMatchOsTheme: true // default: true
+         }
+
+         const darkmode = new Darkmode(options);
+         darkmode.showWidget();
