cb2a5ba69d693e8206cc6b5218384289438b67bd
zozoti3 <szitizoli@gmail.com>
2020-08-24 16:33:13 +0200

    translation LA1 done

--- js/app_properties.js
+++ js/app_properties.js
@@ -1,15 +1,26 @@
-         const options = {
-            bottom: '64px', // default: '32px'
-            right: '32px', // default: '32px'
-            time: '0s', // default: '0.3s'
-            mixColor: '#eeeeee', // default: '#fff'
-            backgroundColor: '#eeeeee',  // default: '#fff'
-            buttonColorDark: '#100f2c',  // default: '#100f2c'
-            buttonColorLight: '#eeeeee', // default: '#fff'
-            saveInCookies: true, // default: true,
-            label: '🌓', // default: ''
-            autoMatchOsTheme: true // default: true
-         }
+const options = {
+   bottom: '64px', // default: '32px'
+   right: '32px', // default: '32px'
+   time: '0s', // default: '0.3s'
+   mixColor: '#eeeeee', // default: '#fff'
+   backgroundColor: '#eeeeee',  // default: '#fff'
+   buttonColorDark: '#100f2c',  // default: '#100f2c'
+   buttonColorLight: '#eeeeee', // default: '#fff'
+   saveInCookies: true, // default: true,
+   label: '🌓', // default: ''
+   autoMatchOsTheme: true // default: true
+}
 
-         const darkmode = new Darkmode(options);
-         darkmode.showWidget();
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+// Process translation
+$(function() {
+   $('.translate').click(function() {
+       var lang = $(this).attr('id');
+
+       $('.lang').each(function(index, item) {
+       $(this).text(arrLang[lang][$(this).attr('key')]);
+       });
+   });
+});
