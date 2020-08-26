9df6dc91797f62227bb9ae677d703c6497fe6464
zozoti3 <szitizoli@gmail.com>
2020-08-26 14:37:17 +0200

    B-AJ1 added

---
+++ template-js/B-AJ1.js
@@ -0,0 +1,36 @@
+var arrLang = {
+    'ENG': {
+        'home': 'Home',
+        'course content and reading': 'Course contents and Recomended Reading',
+        'books': 'Textbooks - PDF version',
+        'language': 'Language',
+        'sylabus': 'Conditions for passing the course',
+        'sylabus content': 'Credit - 2 successfully passed tests - min. 28 points - 56% each',
+        'point1': 'To develop students communicative competence with respect to their academic and professional needs.',
+        'point2': 'After having completed the course, the students should be able to:',
+        'point3': '• talk about general and professional topics in the form of spontaneous dialogue and prepared monologue',
+        'point4': '• correctly and appropriately react to the information they hear in speaking',
+        'point5': '• read and understand specialist literature, communicate politely, maintain intercultural spoken and written communication',
+        'point6': '• Grammatical and lexical structures: usage of verb tenses, frequently used propositions and phrasal verbs, differences between general and specialist language, differences in usage of formal and informal language, terminology in electrical engineering and IT',
+        'point7': '• Writing skill: instructions, definitions',
+        'point8': '• Reading comprehension: reading for gist and study reading, reading for finding specific information, guessing meaning from context',
+        'point9': '• Speaking: dialogue on specialist and social occasions'
+    },
+    'SVK': {
+        'home': 'Domov',
+        'course content and reading': 'Stručná osnova a Literatúra',
+        'books': 'Učebnice - PDF verzie',
+        'language': 'Jazyk',
+        'sylabus': 'Podmienky na absolvovanie predmetu',
+        'sylabus content': 'Zápočet - 2 úspešne absolvované testy - každý min. 28 bodov - 56%',
+        'point1': 'Rozvíjať zručnosti ústnej a písomnej komunikácie v oblasti akademických a profesijných potrieb absolventov, B2 stupňa SEER.',
+        'point2': 'Po absolvovaní predmetu by študent mal byť schopný:',
+        'point3': '• čítať s porozumením odbornú literatúru',
+        'point4': '• hovoriť na témy bežného a profesijného života',
+        'point5': '• zvládnuť písomný prejav vo vybraných žánroch',
+        'point6': '• Gramaticko-lexikálne a syntaktické javy: špecifiká používania slovesných časov, frekventované predložkové spojenia, základy syntaxe, rozdiely v používaní všeobecného a odborného štýlu, stupne formálnosti, lexikálne jednotky z oblasti elektrotechniky a informatiky',
+        'point7': '• Písomný prejav: inštrukcie, definície',
+        'point8': '• Práca s textom: informatívne a študijné čítanie, čítanie za účelom nájdenia všeobecnej a špecifickej informácie, odhad významu z kontextu',
+        'point9': '• Ústny prejav: odborný a spoločenský dialóg'
+    }
+};
--- template-js/B-LA1.js
+++ template-js/B-LA1.js
@@ -6,19 +6,20 @@ var arrLang = {
         'calculated examples': 'Calculated Examples',
         'lectures': 'Lectures',
         'language': 'Language',
+        'conditions': 'Conditions for passing the course',
         'sylabus content': 'During the semester there will be credit tests (30%). To acomplish credit a student must obtain at least 15 of 30 points from credit tests, Additional 70 points can be obtained on the final exam. For final grade A at least 92, for B 83, C 74, D 65 and for E 56 points are needed. Students obtain knowledge of fundamentals of linear algebra, matrix algebra, analytic geometry in 3-dimensional space, polynomials and rational functions with real coefficients. They will be able to use elimination methods to solve systems of linear equations.',
         'week1': '1. Systems of linear equations. Gauss and Gauss-Jordan elimination method',
-        'week2': '2. Matrices and determinants.',
-        'week3': '3. Arithmetic in space Rn. Linear (in)dependence, basis.',
-        'week4': '4. Matrix algebra, computatioéon of the inverse matrix.',
-        'week5': '5. Polynomials, roots, the Horner scheme.',
-        'week6': '6. Rational roots of polynomials with integer coefficients.',
-        'week7': '7. Irreducible polynomials, canonical factorizations of polynomials.',
-        'week8': '8. Ratioonal functions, elementary fractioons.',
-        'week9': '9. Vectors in R3, scalar and vector produts.',
+        'week2': '2. Matrices and determinants',
+        'week3': '3. Arithmetic in space Rn. Linear (in)dependence, basis',
+        'week4': '4. Matrix algebra, computatioéon of the inverse matrix',
+        'week5': '5. Polynomials, roots, the Horner scheme',
+        'week6': '6. Rational roots of polynomials with integer coefficients',
+        'week7': '7. Irreducible polynomials, canonical factorizations of polynomials',
+        'week8': '8. Ratioonal functions, elementary fractioons',
+        'week9': '9. Vectors in R3, scalar and vector produts',
         'week10': '10. Equations of lines and planes',
-        'week11': '11. Quadratic forms.',
-        'week12': '12. quadric surfaces'
+        'week11': '11. Quadratic forms',
+        'week12': '12. Quadric surfaces'
     },
     'SVK': {
         'home': 'Domov',
@@ -27,6 +28,7 @@ var arrLang = {
         'calculated examples': 'Vyrátané Príklady',
         'lectures': 'Prednášky',
         'language': 'Jazyk',
+        'conditions': 'Podmienky na absolvovanie predmetu',
         'sylabus content': 'V priebehu semestra budú písomné previerky spolu 30 bodov, na získanie prístupu na skúšku treba získať spolu najmenej 15 bodov. Na skúške sa dá získať ešte 70 bodov. Na hodnotenia A je potrebné získať v súčte najmenej 92 bodov, na získanie hodnotenia B najmenej 83 bodov, na hodnotenie C najmenej 74 bodov, na hodnotenie D najmenej 65 bodov a na hodnotenie E najmenej 56 bodov. Študent získa základné vedomosti z lineárnej algebry, maticovej algebry, analytickej geometrie v 3-rozmernom priestore a o polynómoch a racionálnych funkciách s reálnymi koeficientami. Dokáže používať eliminačné metódy riešenia sústav lineárnych rovníc.',
         'week1': '1. Sústavy lineárnych rovníc. Gaussova a Gauss-Jordanova eliminačná metóda',
         'week2': '2. Matice a determinanty',
@@ -41,4 +43,4 @@ var arrLang = {
         'week11': '11. Kvadratické formy',
         'week12': '12. Kvadratické plochy'
     }
-    };
+};
