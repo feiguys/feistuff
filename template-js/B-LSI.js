25a09674c73cd4d4fff629e5f743ada5a18dd8ca
zozoti3 <szitizoli@gmail.com>
2020-08-26 18:14:23 +0200

    LSI added

---
+++ template-js/B-LSI.js
@@ -0,0 +1,26 @@
+var arrLang = {
+    'ENG': {
+        'home': 'Home',
+        'course content and reading': 'Course contents and Recomended Reading',
+        'exercises': 'Exercises',
+        'calculated examples': 'Calculated Examples',
+        'lectures': 'Lectures',
+        'language': 'Language',
+        'conditions': 'Conditions for passing the course',
+        'sylabus content': 'There will be one written test for 40 points during the semester. Each student has to earn at least 22 points over the test.Then, a student has to pass a written exam for the maximum of 60 points. In order to get the credits for the subject, a student has to gain at least 56 points, altogether for the test and the exam.',
+        'point0': 'By completion of this subject, a student will gain knowledge of Boolean algebra, forms and minimization of logic functions, and implementation of basic and complex logic functions using logic gates. He/she will also understand the principles and main parameters of logic circuits realized using switches as well as the main principles of combinational and sequential logic circuits. A student will be able to model, analyze and design basic logic circuits.',
+        'point1': "Boolean algebra and Boolean functions. Normal conjunctive and normal disjunctive forms. De Morgan's theorems. Minimization of logic functions. Basic and complex logic gates. Combinational logic circuits. Realization of logic functions using switches. Implementation of basic and complex logic gates in CMOS technology. Memory elements - latches and flip-flop registers. Sequential logic circuits. Finite state machines. Synchronous and asynchronous sequential circuits."
+    },
+    'SVK': {
+        'home': 'Domov',
+        'course content and reading': 'Stručná osnova a Literatúra',
+        'exercises': 'Cvičenia',
+        'calculated examples': 'Vyrátané Príklady',
+        'lectures': 'Prednášky',
+        'language': 'Jazyk',
+        'conditions': 'Podmienky na absolvovanie predmetu',
+        'sylabus content': 'V priebehu semestra bude písomná previerka za 40 bodov. K získaniu prístupu na skúšku je nevyhnutné získať z písomného testu aspoň 22 bodov. Po ukončení semestra je potrebné absolvovať písomnú skúšku za max. 60 bodov. Kredity za predmet sa neudelia študentovi, ktorý získa spolu za cvičenia a skúšku menej ako 56 bodov.',
+        'point0': 'Úspešným absolvovaním predmetu študent získa znalosti z oblasti Booleovskej algebry, formy zápisu a minimalizácie logických funkcií, ako aj ich implementácie pomocou logických členov. Taktiež bude rozumieť princípu činnosti a základným parametrom logických obvodov realizovaných pomocou spínačov, ako aj stavbe a vlastnostiam kombinačných a sekvenčných logických obvodov. Ako absolvent predmetu bude schopný modelovať, navrhnúť a analyzovať základné logické obvody.',
+        'point1': 'Booleovské funkcie. Booleovské výrazy a ich normálne formy. Formy zápisu logických funkcií. Minimalizácia booleovských výrazov. Primitívne a komplexné logické členy. Kombinačné logické obvody. Realizácia logických funkcií pomocou spínačov. Realizácia primitívnych a komplexných logických funkcií v technológii CMOS. Pamäťové elementy/preklápacie obvody. Konečné automaty. Časovanie sekvenčných obvodov. Synchrónne a asynchrónne sekvenčné obvody.'
+    }
+};
